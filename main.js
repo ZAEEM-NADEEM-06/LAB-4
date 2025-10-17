1efb04ae23107f7d98cb1b38c5eb6a81c9e398e8
MUHAMMAD ZAEEM NADEEM <zaeemnadeem2006@gmail.com>
2025-10-17 12:37:10 +0500

    DONE

---
+++ main.js
@@ -0,0 +1,4 @@
+import {add,subtract} from './script.js';
+
+console.log("Addition: " + add(5, 3));
+console.log("Subtraction: " + subtract(5, 3));
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let prompt_name = prompt("Enter your name:");
-const prompt_age = prompt("Enter your age:");
-alert("Hello, " + prompt_name + "! You are " + prompt_age + " years old.");
+//   let prompt_name = prompt("Enter your name:");
+//   const prompt_age = prompt("Enter your age:");
+//   alert("Hello, " + prompt_name + "! You are " + prompt_age + " years old.");
 
 const arrow_func = (my_name) => { console.log(`Hello, ${my_name}! Welcome back.`) };
 console.log(arrow_func("ZAEEM"));
@@ -30,3 +30,30 @@ const obj={
 let arr=[...Object.values(obj), "IT", "2025"];
 console.log(arr);
 
+
+const hi = async() => 
+{
+    const wait = (ms) => new Promise(res => setTimeout(res, ms));
+    await wait(500);
+    return "I performed async operation";
+}
+console.log(hi());
+
+
+async function fetchData() 
+{
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const data = await response.json();
+    console.log(data.title);
+}
+fetchData();
+
+export function add(x, y)
+{
+    return x + y;
+}
+
+export function subtract(x, y)
+{
+    return x - y;
+}
